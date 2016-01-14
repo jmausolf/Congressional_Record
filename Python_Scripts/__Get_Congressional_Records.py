fc62a1219b66dd10cf151bb482f1bb9ed74e1ead
Joshua Mausolf <Joshua@Joshuas-iMac.attlocal.net>
2016-01-13 19:58:42 -0600

    Cleaned up script. Added function descriptions.

--- Python_Scripts/__Get_Congressional_Records.py
+++ Python_Scripts/__Get_Congressional_Records.py
@@ -6,25 +6,31 @@
 ###                            ###
 ##################################
 
-#Could possibly also look at VP, Press Conferences, etc
+
 
 def getURL(date):
-
-	#base_url = "http://www.whitehouse.gov/briefing-room/Speeches-and-Remarks/"+year+"/"+month
-	#base_url = "https://www.gpo.gov/fdsys/pkg/CREC-"+year+"-"+month+"-"+day+"/pdf/CREC-"+year+"-"+month+"-"+day+".pdf"
-
+	"""Prints Congressional Record URL (of PDF record) for a given date."""
 	base_url = "https://www.gpo.gov/fdsys/pkg/CREC-"+date+"/pdf/CREC-"+date+".pdf"
-
-	#https://www.gpo.gov/fdsys/pkg/CREC-2011-09-21/pdf/CREC-2011-09-21.pdf
 	print base_url
 
 
 def getfullURL(date):
+	"""Returns Congressional Record URL (of PDF record) for a given date."""
 	base_url = "https://www.gpo.gov/fdsys/pkg/CREC-"+date+"/pdf/CREC-"+date+".pdf"
 	return base_url
 
 
 def getdates(yr1=2009, yr2=2016, month1=1, month2=12, day1=1, day2=31):
+	"""Returns a list of all possible dates as a set of tuples:
+		E.G. date1 = (2009, 01, 01) or datex = (2011, 12, 06)
+		The form is therefore a [(yyyy, mm, dd), ... (yyyy, mm, dd)]
+
+		This function is set up to return all possible dates between 01/01/2009 and 12/31/2016. 
+
+		It can be modified to provide expanded dates if need be.
+
+		Note that while this creates dates that do not exist, the end result
+		is simply an invalid url link, which is handled in subsequent functions."""
 
 	import itertools
 
@@ -44,108 +50,76 @@ def getdates(yr1=2009, yr2=2016, month1=1, month2=12, day1=1, day2=31):
 	req_months = months[index_mo1:index_mo2+1]
 	req_days = days[index_dy1:index_dy2+1]
 
-	#x = (list(itertools.product(req_years, req_months)))
-	x = (list(itertools.product(req_years, req_months, req_days)))
-	#print x
-	return x
+	dates = (list(itertools.product(req_years, req_months, req_days)))
+	return dates
 
-#getdates()
 
 def getcongressURLs():
+	"""This function returns a list of urls for the congressional records between 2009 and 2016."""
 
-	#x = (list(itertools.product(req_years, req_months)))
-	x = getdates()
+	dates = getdates()
 
 	f = open('congressional_records_URLs2.csv', 'w')
 	try:
-		for dt in range(0, len(x)):
-			date = str(x[dt]).replace("'", "").replace(",", "-").replace(" ", "").replace("(", "").replace(")", "")
-			#print date
-			full_url = getfullURL(date)
-			print full_url
-			f.write(u'%s\n' % (full_url))
-	finally:
-		f.close()
-
-#getcongressURLs()
-
-
-def getparentURLs(yr1=2009, yr2=2016, month1=1, month2=12, day1=1, day2=31):
-
-	import itertools
-
-	years = ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016']
-	months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-	days = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
-
-
-	index_yr1 = yr1-2009
-	index_yr2 = yr2-2009
-	index_mo1 = month1-1
-	index_mo2 = month2-1
-	index_dy1 = day1-1
-	index_dy2 = day2-1
-
-	req_years = years[index_yr1:index_yr2+1]
-	req_months = months[index_mo1:index_mo2+1]
-	req_days = days[index_dy1:index_dy2+1]
-
-	#x = (list(itertools.product(req_years, req_months)))
-	x = (list(itertools.product(req_years, req_months, req_days)))
-
-	f = open('congressional_records_URLs.csv', 'w')
-	try:
-		for dt in range(0, len(x)):
-			date = str(x[dt]).replace("'", "").replace(",", "-").replace(" ", "").replace("(", "").replace(")", "")
+		for dt in range(0, len(dates)):
+			date = str(dates[dt]).replace("'", "").replace(",", "-").replace(" ", "").replace("(", "").replace(")", "")
 			full_url = getfullURL(date)
 			f.write(u'%s\n' % (full_url))
 	finally:
 		f.close()
 
-
-#getparentURLs()
-
-import signal
-
-class TimeoutException(Exception):   # Custom exception class
-    pass
-
-def timeout_handler(signum, frame):   # Custom signal handler
-    raise TimeoutException
-
-# Change the behavior of SIGALRM
-signal.signal(signal.SIGALRM, timeout_handler)
 
 
 def get_congressional_records():
+	"""This function retrieves the congressional records for the possible dates between 2009 and 2016.
 
-	import sys, csv, time, os, subprocess, wget
+	It writes urls where exceptions occur to a seperate spreadsheet.
+	Note that pages where no congressional record exists yield a 404 page for the Congressional record 
+	website. As such, the wget function downloads the html code for that page, saving it based
+	on the url as a pdf:
+
+	"https://www.gpo.gov/fdsys/pkg/CREC-"+date+"/pdf/CREC-"+date+".pdf"
+
+	As a result, these pages are all identical and have a size of approximately 5618 bytes. 
+	A subsequent bash script filters these false hits out based on these parameters.
+
+	Note that the congressional record site will block your bot from downloading the page
+	if the time clock is too fast. 
+
+	For example, time.sleep(0.5) quickly locks up. The current set time time.sleep(5.5) 
+	has run in testing without issue. Experiment at your own risk.
+
+	"""
+
+	import sys, csv, time, os, subprocess, wget, signal
+
 	sys.path.append('../Python_Scripts/')
 	sys.path.append('..')
 
-	#import wget
-    #url = 'http://www.futurecrew.com/skaven/song_files/mp3/razorback.mp3'
+	## Add Functions for Timeout and Signal ##
+	class TimeoutException(Exception):
+		pass
 
-    #filename = wget.download(url)
+	def timeout_handler(signum, frame):
+	    raise TimeoutException
 
-	#os.chdir('bash_Speech')
-	#os.chdir('Speech_President')
+	signal.signal(signal.SIGALRM, timeout_handler)
+
+
+	## Setup Directory and Dates ##
 	try:
 		os.mkdir("Congressional_Records")
 	except:
 		pass
 
 	os.chdir("Congressional_Records")
-	#getcongressURLs()
 
-	x = getdates()
-	#print x
+	dates = getdates()
 
-	#f = open('congressional_records_URLs2.csv', 'w')
+	## Begin Congressional Record Collection ##
 	try:
-		for dt in range(0, len(x)):
-		#for dt in range(300, 350):
-			date = str(x[dt]).replace("'", "").replace(",", "-").replace(" ", "").replace("(", "").replace(")", "")
+		for dt in range(0, len(dates)):
+			date = str(dates[dt]).replace("'", "").replace(",", "-").replace(" ", "").replace("(", "").replace(")", "")
 			url = str(getfullURL(date))
 			print "Downloading congressional record for date: ", date
 
@@ -161,7 +135,7 @@ def get_congressional_records():
 						f.write(u'%s\n' % (url))
 				finally:
 					f.close()
-				continue # continue the for loop if function A takes more than 5 second
+				continue
 
 			except:
 				time.sleep(5.5)
@@ -183,10 +157,7 @@ def get_congressional_records():
 			f.close()
 		pass
 
-
-
-
-
+##Run Get Congressional Records
 get_congressional_records()
 
 
